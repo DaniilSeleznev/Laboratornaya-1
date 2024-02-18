35829d8196bb4b9decfcf3c76ad7f67c81508385
DaniilSeleznev <148125578+DaniilSeleznev@users.noreply.github.com>
2024-02-18 23:05:57 +0300

    Add files via upload

---
+++ lab_2.1.py
@@ -0,0 +1,46 @@
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
+
+
+class Book:
+    def __init__(self, id_: int, name: str, pages: int):
+        if not isinstance(id_, int):
+            raise TypeError("ID должен быть типа int")
+        if not isinstance(name, str):
+            raise TypeError("Name должен быть типа str")
+        if not isinstance(pages, int):
+            raise TypeError("Pages должен быть типа int")
+        if id_ < 0:
+            raise ValueError("Id не может быть меньше 0")
+        if pages <= 0:
+            raise ValueError("Pages должен быть больше 0")
+        self.id_ = id_
+        self.name = name
+        self.pages = pages
+
+    def __str__(self) -> str:
+        return f'Книга "{self.name}"'
+
+    def __repr__(self) -> str:
+        return f'Book(id_={self.id_!r}, name={self.name!r}, pages={self.pages!r})'
+
+
+if __name__ == '__main__':
+    # инициализируем список книг
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    for book in list_books:
+        print(book)  # проверяем метод __str__
+
+    print(list_books)  # проверяем метод __repr__
